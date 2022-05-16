0a0f11cfd68f8eeda1b480c4beef96e17e9588a9
Thomas GRATTON <thomas.gratton@ac-amiens.fr>
2022-05-16 15:43:15 +0200

    fix #6

--- walktoncopieur.rb
+++ walktoncopieur.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/ruby -w
 # copieurs.rb
 #
-# Copyright (c) 2022 - Thomas GRATTON <thomas.gratton@ac-amiens.fr>
+# Copyright (c) 2020 - Thomas GRATTON <thomas.gratton@ac-amiens.fr>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -19,94 +19,114 @@
 require 'csv'
 require 'json'
 
-# array MAJ 18012022
-
+# ip des copieurs
 array =[
-'10.2.104.10',
-'10.60.104.10',
-'10.60.105.10',
-'10.80.101.10',
-'10.80.103.10',
-'10.80.103.11',
-'10.64.128.55',
-'172.26.184.29',
-'172.26.184.114',
-'172.30.177.88',
-'172.30.178.138',
-'172.30.178.141',
-'172.30.178.93',
-'172.30.179.199',
-'172.30.179.200',
-'172.30.180.161',
-'172.30.180.28',
-'172.30.180.75',
-'172.30.181.147',
-'172.30.181.47',
-'172.30.182.179',
-'172.30.182.188',
-'172.30.182.190',
-'172.30.182.20',
-'172.30.182.5',
-'172.30.183.233',
-'172.30.183.64',
-'172.30.185.101',
-'172.30.185.104',
-'172.30.185.98',
-'172.30.189.95'
+'172.30.182.20',				#CAB-RECTEUR
+'172.30.182.5',					#SG
+'172.30.181.147',				#DAF-DPS
+'172.30.180.28',				#DEC-Secretariat
+'172.30.180.75',				#DEC
+'172.30.182.188',				#Santé Scolaire 6ème
+'172.30.178.141',				#1er etage DLSa
+'172.30.178.138',				#DEPP
+'172.30.183.64',				#DESR
+'172.30.181.47',				#DAJ-DOS
+'172.30.177.88',				#Secretariat DSIN
+'172.30.178.93',				#DPAE
+'172.30.179.199',				#DPE-4-5
+'172.30.179.200',				#Dpe-2-3
+'172.30.183.233',				#DAFPEN-14AL
+'172.30.180.161',				#IPR 400
+'172.30.182.190',				#DAET
+'172.30.182.179',				#SAIO
+'172.30.185.101',				#IEN 80 E cote dalle
+'172.30.185.98',				#IEN 80 - F - Cote Rue
+'172.30.185.104',				#DSDEN 80 - Staff
+'172.30.189.95',				#DANE
+'10.80.101.10',					#CIO Abbeville
+'10.80.103.11',					#CIO AMIENS SUD
+'10.80.104.10',					#Paio FRIVILLE-ESCARBOTIN
+'10.60.104.10',					#CIO CLERMONT
+'10.60.101.220',				#CIO COMPIEGNE
+'10.60.102.10',					#CIO CREIL
+'10.80.106.50',					#Paio Albert
+'10.2.104.10',					#CIO ST QUENTIN
+'10.2.101.10.',					#CIO Château-Thierry
+'10.80.103.10',					#Cio Amiens Nord
+'10.60.105.10',					#Cio Noyon
+'10.80.81.10',					#Cio Soissons
+'192.168.1.10',					#Cio Beauvais
+'10.64.128.55',					#Cio Péronne
+'10.2.103.10',					#Cio Laon
+'10.64.128.55',					#Circonscription Péronne
+'192.168.1.50',					#Circonscription Abbeville
+'172.26.184.29',				#Sdjes 80
+'172.26.184.114',				#Drajes 80
+'172.26.184.122',				#Drajes 80-1
+'172.26.184.123',				#Drajes 80-2
+'172.26.184.124',				#Drajes 80-3
+'172.30.178.50',				#DPAE-Secretariat
+'172.30.185.20'					#DSDEN 80 - Lecul
 ]
 
 array_eco =[
-'172.30.178.50',
-'172.30.179.181',
-'172.30.185.20'
+'172.30.179.181',				# Dpe1-prive
+'172.30.179.182',				# Secretariat & Direction DPE
+'10.80.104.12',					# Circonscription Abbeville
+'192.168.1.50',					# Circonscription Doullens
+'10.64.128.200'					# Circonscription Montdidier
 ]
 
+# oids a recuperer
 oid = [
-  'iso.3.6.1.2.1.1.6.0',      # nom imprimante
-  'iso.3.6.1.2.1.43.5.1.1.17.1',    # serial
-  'iso.3.6.1.2.1.43.5.1.1.16.1',    # modele
-  'iso.3.6.1.4.1.1347.42.2.5.1.1.3.1',    # total tout cumul
-  'iso.3.6.1.4.1.1347.42.2.1.1.1.7.1.1',  # A3 noir et blanc
-  'iso.3.6.1.4.1.1347.42.2.1.1.1.10.1.1', # A3 couleur niveau 1
-  'iso.3.6.1.4.1.1347.42.2.1.1.1.11.1.1', # A3 couleur niveau 2
-  'iso.3.6.1.4.1.1347.42.2.1.1.1.12.1.1', # A3 couleur niveau 3
-  'iso.3.6.1.4.1.1347.42.2.1.1.1.7.1.3',  # A4 noir et blanc
-  'iso.3.6.1.4.1.1347.42.2.1.1.1.10.1.3', # A4 couleur niveau 1
-  'iso.3.6.1.4.1.1347.42.2.1.1.1.11.1.3', # A4 couleur niveau 2
-  'iso.3.6.1.4.1.1347.42.2.1.1.1.12.1.3', # A4 couleur niveau 3
-  'iso.3.6.1.4.1.1347.42.2.1.1.1.6.1.3',  # totala4
-  'iso.3.6.1.4.1.1347.42.2.1.1.1.6.1.1',  # totala3
-  'iso.3.6.1.2.1.43.11.1.1.9.1.1',    # toner1-numerateur
-  'iso.3.6.1.2.1.43.11.1.1.8.1.1',    # toner2-denominateur
-  'iso.3.6.1.4.1.1347.42.3.1.1.1.1.1',  # A4 imprimees imprimante
-  'iso.3.6.1.4.1.1347.42.3.1.1.1.1.2',  # A4 imprimees copier
-  'iso.3.6.1.4.1.1347.42.3.1.1.1.1.4',  # A4 imprimees fax
-  'iso.3.6.1.4.1.1347.42.3.1.3.1.1.2',  # pages numerisees copier
-  'iso.3.6.1.4.1.1347.42.3.1.3.1.1.4'  # pages numerisees fax
+  'iso.3.6.1.2.1.1.6.0',			# nom imprimante
+  'iso.3.6.1.2.1.4.20.1.1',			# ip
+  'iso.3.6.1.2.1.43.5.1.1.17.1',		# serial
+  'iso.3.6.1.2.1.43.5.1.1.16.1',		# modele
+  'iso.3.6.1.4.1.1347.43.10.1.1.12.1.1',	# total tout cumul
+  'iso.3.6.1.4.1.1347.42.2.1.1.1.7.1.1',	# A3 noir et blanc
+  'iso.3.6.1.4.1.1347.42.2.1.1.1.10.1.1',	# A3 couleur
+  'iso.3.6.1.4.1.1347.42.2.1.1.1.7.1.3',	# A4 noir et blanc
+  'iso.3.6.1.4.1.1347.42.2.1.1.1.10.1.3',	# A4 couleur
+  'iso.3.6.1.4.1.1347.42.2.1.1.1.6.1.3',	# totala4
+  'iso.3.6.1.4.1.1347.42.2.1.1.1.6.1.1'		# totala3
+#  'iso.3.6.1.2.1.43.11.1.1.9.1.1',		# toner1-numerateur
+#  'iso.3.6.1.2.1.43.11.1.1.8.1.1',		# toner2-denominateur
+#  'iso.3.6.1.4.1.1347.42.3.1.1.1.1.1',		# A4 imprimees imprimante
+#  'iso.3.6.1.4.1.1347.42.3.1.1.1.1.2',		# A4 imprimees copier
+#  'iso.3.6.1.4.1.1347.42.3.1.1.1.1.4',		# A4 imprimees fax
+#  'iso.3.6.1.4.1.1347.42.3.1.3.1.1.2',		# pages numerisees copier
+#  'iso.3.6.1.4.1.1347.42.3.1.3.1.1.4'		# pages numerisees fax
 ]
 
 oid_eco = [
-  'iso.3.6.1.2.1.1.6.0',      # nom imprimante
-  'iso.3.6.1.2.1.43.5.1.1.17.1',    # serial
-  'iso.3.6.1.2.1.43.5.1.1.16.1',    # modele
-  'iso.3.6.1.4.1.1347.42.2.5.1.1.3.1',    # total tout cumul
-  'iso.3.6.1.4.1.1347.42.2.1.1.1.7.1.1',  # A3 noir et blanc
-  'iso.3.6.1.4.1.1347.42.2.1.1.1.10.1.1', # A3 couleur niveau 1
-  'iso.3.6.1.4.1.1347.42.2.1.1.1.11.1.1', # A3 couleur niveau 2
-  'iso.3.6.1.4.1.1347.42.2.1.1.1.12.1.1', # A3 couleur niveau 3
-  'iso.3.6.1.4.1.1347.42.2.1.1.1.7.1.3',  # A4 noir et blanc
-  'iso.3.6.1.4.1.1347.42.2.1.1.1.10.1.3', # A4 couleur niveau 1
-  'iso.3.6.1.4.1.1347.42.2.1.1.1.11.1.3', # A4 couleur niveau 2
-  'iso.3.6.1.4.1.1347.42.2.1.1.1.6.1.3',  # totala4
-  'iso.3.6.1.4.1.1347.42.2.1.1.1.6.1.1',  # totala3
-  'iso.3.6.1.2.1.43.11.1.1.9.1.1',    # toner1-numerateur
-  'iso.3.6.1.2.1.43.9.2.1.17.1.1',
-  'iso.3.6.1.2.1.43.11.1.1.8.1.1',    # toner2-denominateur
-  'iso.3.6.1.4.1.1347.42.3.1.1.1.1.1',  # A4 imprimees imprimante
-  'iso.3.6.1.4.1.1347.42.3.1.1.1.1.2',  # A4 imprimees copier
-  'iso.3.6.1.4.1.1347.42.3.1.1.1.1.4',  # A4 imprimees fax
-  'iso.3.6.1.4.1.1347.42.3.1.3.1.1.2',  # pages numerisees copier
-  'iso.3.6.1.4.1.1347.42.3.1.3.1.1.4'  # pages numerisees fax
+  'iso.3.6.1.2.1.1.6.0',			# nom imprimante
+  'iso.3.6.1.2.1.4.20.1.1',			# ip
+  'iso.3.6.1.2.1.43.5.1.1.17.1',		# serial
+  'iso.3.6.1.2.1.43.5.1.1.16.1',		# modele
+  'iso.3.6.1.4.1.1347.43.10.1.1.12.1.1',	# total tout cumul
+  'iso.3.6.1.4.1.1347.42.2.1.1.1.7.1.1',	# A3 noir et blanc
+  'iso.3.6.1.4.1.1347.42.2.1.1.1.10.1.1',	# A3 couleur
+  'iso.3.6.1.4.1.1347.42.2.1.1.1.7.1.3',	# A4 noir et blanc
+  'iso.3.6.1.4.1.1347.42.2.1.1.1.10.1.3',	# A4 couleur
+  'iso.3.6.1.4.1.1347.42.2.1.1.1.6.1.3',	# totala4
+  'iso.3.6.1.4.1.1347.42.2.1.1.1.6.1.1'		# totala3
+#  'iso.3.6.1.4.1.1347.42.2.1.1.1.7.1.1',	# A3 noir et blanc
+#  'iso.3.6.1.4.1.1347.42.2.1.1.1.10.1.1',	# A3 couleur niveau 1
+#  'iso.3.6.1.4.1.1347.42.2.1.1.1.11.1.1',	# A3 couleur niveau 2
+#  'iso.3.6.1.4.1.1347.42.2.1.1.1.12.1.1',	# A3 couleur niveau 3
+#  'iso.3.6.1.4.1.1347.42.2.1.1.1.10.1.3',	# A4 couleur niveau 1
+#  'iso.3.6.1.4.1.1347.42.2.1.1.1.11.1.3',	# A4 couleur niveau 2
+#  'iso.3.6.1.4.1.1347.42.2.1.1.1.6.1.3',	# totala4
+#  'iso.3.6.1.4.1.1347.42.2.1.1.1.6.1.1',	# totala3
+#  'iso.3.6.1.2.1.43.11.1.1.9.1.1',		# toner1-numerateur
+#  'iso.3.6.1.2.1.43.9.2.1.17.1.1',
+#  'iso.3.6.1.2.1.43.11.1.1.8.1.1',		# toner2-denominateur
+#  'iso.3.6.1.4.1.1347.42.3.1.1.1.1.1',		# A4 imprimees imprimante
+#  'iso.3.6.1.4.1.1347.42.3.1.1.1.1.2',		# A4 imprimees copier
+#  'iso.3.6.1.4.1.1347.42.3.1.1.1.1.4',		# A4 imprimees fax
+#  'iso.3.6.1.4.1.1347.42.3.1.3.1.1.2',		# pages numerisees copier
+#  'iso.3.6.1.4.1.1347.42.3.1.3.1.1.4'		# pages numerisees fax
 ]
 
 def notify_slack(webhook_url, channel, username, text, image)
@@ -121,7 +141,7 @@ def notify_slack(webhook_url, channel, username, text, image)
 end
 
 CSV.open("stats-copieurs.csv", "wb", {:col_sep => ";"}) do |csv|
-  csv << ["nom_imprimante", "num_serie", "modele", "total_cumul", "a3_nb", "a3_niveau1", "a3_niveau2", "a3_niveau3", "a4_nb", "a4_niveau1", "a4_niveau2", "a4_niveau3", "totala4", "totala3", "toner1-numerateur", "toner2-denominateur","a4_imprimees_src_imprimante","a4_imprimees_src_copier","a4_imprimees_src_fax","pages_numerisees_src_copier","pages_numerisees_src_fax"]
+  csv << ["nom_imprimante", "adresse_ip", "num_serie", "modele", "total_cumul", "a3_nb", "a3_couleur", "a4_nb", "a4_couleur", "totala4", "totala3"]
   csv << []
   array.each do |ipcopieur| 
     v = []
@@ -147,12 +167,12 @@ time = Time.new
 date = time.strftime("%d-%m-%Y @ %H:%M:%S")
 command = `sed -i -e 's/\"\"\"//g' stats-copieurs.csv`
 command1 = `zip stats-copieurs.zip stats-copieurs.csv`
-command2 = `uuencode stats-copieurs.zip stats-copieurs.zip | mail -s "Stats copieurs - #{date}" rt@ac-amiens.fr`
+command2 = `uuencode stats-copieurs.zip stats-copieurs.zip | mail -s "Stats copieurs - #{date}" tg@ac-amiens.fr`
 
 notify_slack(
   'https://ratatouille.in.ac-amiens.fr:8065/hooks/dc8d79ju4pdz7jk3sraxjtpj4h',
   'rapport-conso-kyocera',
   'kyocera',
-  'Rapport de consommation des copieurs généré et envoyé à RT.',
+  'Rapport de consommation des copieurs généré et envoyé à TG.',
   'https://ratatouille.in.ac-amiens.fr:8065/static/stats1.png',
 )
