499b852f747ed5ce048f8397e9c7d72966494a87
Thomas GRATTON <thomas.gratton@ac-amiens.fr>
2023-09-20 10:45:46 +0200

    hello world

--- walktoncopieur.rb
+++ walktoncopieur.rb
@@ -1,8 +1,6 @@
 #!/usr/bin/ruby -w
 # copieurs.rb
 #
-# Copyright (c) 2022 - Thomas GRATTON <thomas.gratton@ac-amiens.fr>
-#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
@@ -21,59 +19,11 @@ require 'json'
 
 # ip des copieurs
 array =[
-'172.30.182.20',				#CAB-RECTEUR
-'172.30.182.5',					#SG
-'172.30.181.147',				#DAF-DPS
-'172.30.180.28',				#DEC-Secretariat
-'172.30.180.75',				#DEC
-'172.30.182.188',				#Santé Scolaire 6ème
-'172.30.178.141',				#1er etage DLSa
-'172.30.178.138',				#DEPP
-'172.30.183.64',				#DESR
-'172.30.181.47',				#DAJ-DOS
-'172.30.177.88',				#Secretariat DSIN
-'172.30.178.93',				#DPAE
-'172.30.179.199',				#DPE-4-5
-'172.30.179.200',				#Dpe-2-3
-'172.30.183.233',				#DAFPEN-14AL
-'172.30.180.161',				#IPR 400
-'172.30.182.190',				#DAET
-'172.30.182.179',				#SAIO
-'172.30.185.101',				#IEN 80 E cote dalle
-'172.30.185.98',				#IEN 80 - F - Cote Rue
-'172.30.185.104',				#DSDEN 80 - Staff
-'172.30.189.95',				#DANE
-'10.80.101.10',					#CIO Abbeville
-'10.80.103.11',					#CIO AMIENS SUD
-'10.80.104.10',					#Paio FRIVILLE-ESCARBOTIN
-'10.60.104.10',					#CIO CLERMONT
-'10.60.101.220',				#CIO COMPIEGNE
-'10.60.102.10',					#CIO CREIL
-'10.80.106.50',					#Paio Albert
-'10.2.104.10',					#CIO ST QUENTIN
-'10.68.9.10',					#CIO Château-Thierry
-'10.80.103.10',					#Cio Amiens Nord
-'10.60.105.10',					#Cio Noyon
-'10.80.81.10',					#Cio Soissons
-'10.60.106.104',				#Cio Beauvais
-'10.64.128.55',					#Cio Péronne
-'10.2.103.10',					#Cio Laon
-'10.64.128.55',					#Circonscription Péronne
-'172.26.184.29',				#Sdjes 80
-'172.26.184.114',				#Drajes 80
-'172.26.184.122',				#Drajes 80-1
-'172.26.184.123',				#Drajes 80-2
-'172.26.184.124',				#Drajes 80-3
-'172.30.178.50',				#DPAE-Secretariat
-'172.30.185.20'					#DSDEN 80 - Lecul
+'0.0.0.0',				#intitulé
 ]
 
 array_eco =[
-'172.30.179.181',				# Dpe1-prive
-'172.30.179.182',				# Secretariat & Direction DPE
-'10.80.104.12',					# Circonscription Abbeville
-'192.168.1.50',					# Circonscription Doullens
-'10.64.128.200'					# Circonscription Montdidier
+'0.0.0.0',				#intitulé
 ]
 
 # oids a recuperer
@@ -166,13 +116,13 @@ time = Time.new
 date = time.strftime("%d-%m-%Y @ %H:%M:%S")
 command = `sed -i -e 's/\"\"\"//g' stats-copieurs.csv`
 command1 = `zip stats-copieurs.zip stats-copieurs.csv`
-#command2 = `uuencode stats-copieurs.zip stats-copieurs.zip | mail -s "Stats copieurs - #{date}" tg@ac-amiens.fr`
-command2 = `uuencode stats-copieurs.zip stats-copieurs.zip | mail -s "Stats copieurs - #{date}" tg@ac-amiens.fr -a stats-copieurs.zip`
+#command2 = `uuencode stats-copieurs.zip stats-copieurs.zip | mail -s "Stats copieurs - #{date}" your@mail.com`
+command2 = `uuencode stats-copieurs.zip stats-copieurs.zip | mail -s "Stats copieurs - #{date}" your@mail.com -a stats-copieurs.zip`
 
 notify_slack(
-  'https://ratatouille.in.ac-amiens.fr:8065/hooks/dc8d79ju4pdz7jk3sraxjtpj4h',
+  'https://yoururl/specificalphanumchain',
   'rapport-conso-kyocera',
   'kyocera',
-  'Rapport de consommation des copieurs généré et envoyé à TG.',
-  'https://ratatouille.in.ac-amiens.fr:8065/static/stats1.png',
+  'Your message',
+  'https://yoururl/pathtoimage',
 )
